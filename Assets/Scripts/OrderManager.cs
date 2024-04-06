8264405ac93bb82343fc3907ec6546762c4bc872
CarterAWebb827 <CarterAWebb827@gmail.com>
2024-04-06 13:06:43 -0700

    Added Pizza/Order Manager

---
+++ Assets/Scripts/OrderManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderManager : MonoBehaviour{
+    /// <summary>
+    /// This script manages the order making process.
+    /// 
+    /// It should:
+    /// - Generate a random order and time limit
+    /// - Update the UI with the order and time limit
+    /// </summary>
+}
---
+++ Assets/Scripts/PizzaManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaManager : MonoBehaviour {
+    /// <summary>
+    /// This script manages the pizza making process.
+    /// 
+    /// It should:
+    /// - Keep track of the current pizza being made (in a list?)
+    /// - Check (when done) if the pizza is correct
+    /// - Keep track of the score
+    /// - Subtract points for mistakes (extra toppings, wrong toppings, etc.)
+    /// - Subtract points for taking too long
+    /// 
+    /// The user should be able to drag and drop toppings onto the pizza which
+    /// will be placed on top of the last thing they dropped it on. The topping models
+    /// will be associated with a simplified game object that will be placed on the pizza.
+    /// </summary>
+}
