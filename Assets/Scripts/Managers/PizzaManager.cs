1f9f306a7511c9ebfd0f1037485e3381fb376589
CarterAWebb827 <CarterAWebb827@gmail.com>
2024-04-07 14:55:32 -0700

    Added Simplified Ingredients

--- Assets/Scripts/Managers/PizzaManager.cs
+++ Assets/Scripts/Managers/PizzaManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PizzaManager : MonoBehaviour {
     /// <summary>
@@ -17,4 +18,19 @@ public class PizzaManager : MonoBehaviour {
     /// will be placed on top of the last thing they dropped it on. The topping models
     /// will be associated with a simplified game object that will be placed on the pizza.
     /// </summary>
+    
+    // An array of tuples of game objects and their associated simplified game objects
+    private GameObject[,] pizzaModelWithSimplified;
+
+    [Header("Toppings")]
+    [SerializeField] public GameObject[] toppings;
+    [SerializeField] public GameObject[] simplifiedToppings;
+
+    private void Start() {
+        pizzaModelWithSimplified = new GameObject[toppings.Length, 2];
+        for (int i = 0; i < toppings.Length; i++) {
+            pizzaModelWithSimplified[i, 0] = toppings[i];
+            pizzaModelWithSimplified[i, 1] = simplifiedToppings[i];
+        }
+    }
 }
